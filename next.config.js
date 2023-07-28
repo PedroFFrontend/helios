4dcedd2cd612b9dae81451c333703b522ab0ec0c
PedroFFrontend <140765475+PedroFFrontend@users.noreply.github.com>
2023-07-28 12:44:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
-    swcMinify:false
+    // output:"export",
+    // swcMinify:false
 }
 
 module.exports = nextConfig
